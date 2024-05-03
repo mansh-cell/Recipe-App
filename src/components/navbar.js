dd119c277e316bdcba3cfc5cf51a01e0a4578104
manshi-cell <mishramanshi14@gmail.com>
2024-05-04 00:09:52 +0530

    hi

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+export default function Navbar(){
+    return(
+        <div className="header container">
+            <a href="#!" className="logo">Y<span>um</span>Spot</a>
+            <div className="nav-links">
+                <a href="#!">Home</a>
+                <a href="#!" >Recipes</a>
+                <a href="#!" >Settings</a>
+            </div>
+            <div className="sidebar-btn">
+                
+
+            </div>
+        </div>
+    )
+
+}
